37a313e844bd7cc4e5a4d0f49066bd3f671d1a07
tarunchhabra26 <tarun.chhabra@gmail.com>
2015-10-07 19:21:01 -0400

    Changed the search query

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,7 @@ class Book < ActiveRecord::Base
   def self.search(a)
     if a
 
-  	  self.where("title like ? or isbn like ? or description like ? or author like ? or status like ?", "%#{a}%", "%#{a}%", "%#{a}%", "%#{a}%", "%#{a}%")
+  	  self.where("title like ? or isbn like ? or description like ? or author like ? or cast(status as text) like ?", "%#{a}%", "%#{a}%", "%#{a}%", "%#{a}%", "%#{a}%")
 
     end
   end
