da5a74a9c33604db62c01065a4b678b7189ba7d4
zeal <ganatrazeal@gmail.com>
2015-10-07 18:22:20 -0400

    added rec controler

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,9 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save User without email" do
+    user = User.new
+    assert_not user.save
+  end
 end
+
